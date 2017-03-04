557161b8169d7b3b3365a201e287864a1a092a1f
n3vjppro <n3vjppro@gmail.com>
2017-03-04 18:12:36 +0700

    eeee

---
+++ app/src/main/java/com/example/administrator/myapplication/acx.java
@@ -0,0 +1,21 @@
+package com.example.administrator.myapplication;
+
+/**
+ * Created by Administrator on 3/1/2017.
+ */
+
+public class acx {
+    String a;
+
+    public acx(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
